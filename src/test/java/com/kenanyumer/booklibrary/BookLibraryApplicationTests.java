9e7fc24e4e5eec1a163f4702e5ea489a809b0454
Kenan Yumer <kenan.yumer.etu@univ-lille.fr>
2026-09-07 17:40:43 +0200

    Initial commit

---
+++ src/test/java/com/kenanyumer/booklibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenanyumer.booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
