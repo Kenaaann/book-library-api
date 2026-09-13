8ab629c1c3d7c38b9736bd9ba4de5d568be1b9c2
Kenan <kenanyumer@gmail.com>
2026-09-13 21:48:21 +0200

    Changed package of the Application to do it more like the convention

--- src/main/java/com/kenanyumer/booklibrary/configuration/BookLibraryApplication.java
+++ src/main/java/com/kenanyumer/booklibrary/configuration/BookLibraryApplication.java
@@ -1,10 +1,9 @@
-package com.kenanyumer.booklibrary;
+package com.kenanyumer.booklibrary.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication(scanBasePackages = {
         "com.kenanyumer.booklibrary.book", "com.kenanyumer.booklibrary"
